81ca3eeb3ca9d74bea609d2d3b5163e993bd37a5
Andy <ahalterman0@gmail.com>
2016-03-04 23:38:17 -0500

    Basic extract and transform capability

--- extract_XML.py
+++ extract_XML.py
@@ -48,3 +48,44 @@ def extract_xml(text):
         "doc_id" : doc_id
     }
     return output
+
+
+def get_file_list(root_dir):
+    files = [] #Will have list of all the files parsed through
+    for dname, subdirlist, flist in os.walk(root_dir):
+        for fname in flist:
+            files.append(os.path.join(dname, fname))
+    return files
+
+
+def process_file_list(files):
+    docs = []
+    for fi in files:
+        with open(fi,'r') as f:
+            for row_num, row in enumerate(f):
+                if row[0:5] == "<?xml":
+                    d = {"filename" : fi,
+                         "row_num" : row_num,
+                         "xml" : row}
+                    docs.append(d)
+
+    processed = []
+    for i, dd in enumerate(docs):
+        try:
+            processed.append(extract_xml(dd['xml']))
+        except IndexError:
+            print i,
+            print dd['filename']
+            print dd['xml']
+        except AttributeError as e:
+            print i,
+            print e
+            print dd['filename']
+            print dd['xml']
+    return processed
+
+if __name__ == "__main__":
+    ln_files = get_file_list("/phani_event_data")
+    short_files = ln_files[0:100]
+    processed = process_file_list(short_files)
+    print len(processed)
