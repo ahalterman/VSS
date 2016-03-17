aae0a9c8372a414a329b3a20ef542344afb4ae97
Andy Halterman <ahalterman0@gmail.com>
2016-03-17 11:59:01 -0500

    Initial multiprocessing capability

--- extract_XML.py
+++ extract_XML.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import re
 import dateutil.parser
+import os
+from multiprocessing import Pool
 
 def parse_date(raw_date):
     date_string = re.findall(r"(.+\d{4})", raw_date)
@@ -57,7 +59,7 @@ def get_file_list(root_dir):
             files.append(os.path.join(dname, fname))
     return files
 
-
+# serial implementation
 def process_file_list(files):
     docs = []
     for fi in files:
@@ -75,17 +77,46 @@ def process_file_list(files):
             processed.append(extract_xml(dd['xml']))
         except IndexError:
             print i,
-            print dd['filename']
-            print dd['xml']
+            #print dd['filename']
+            #print dd['xml']
         except AttributeError as e:
             print i,
             print e
-            print dd['filename']
-            print dd['xml']
+            #print dd['filename']
+            #print dd['xml']
+    return processed
+
+# parallel implementation
+def process_file(fi):
+    docs = []
+    with open(fi,'r') as f:
+        for row_num, row in enumerate(f):
+            if row[0:5] == "<?xml":
+                d = {"filename" : fi,
+                     "row_num" : row_num,
+                     "xml" : row}
+                docs.append(d)
+
+    processed = []
+    for i, dd in enumerate(docs):
+        try:
+            processed.append(extract_xml(dd['xml']))
+        except IndexError:
+            print i,
+            #print dd['filename']
+            #print dd['xml']
+        except AttributeError as e:
+            print i,
+            print e
+            #print dd['filename']
+            #print dd['xml']
     return processed
 
 if __name__ == "__main__":
     ln_files = get_file_list("/phani_event_data")
     short_files = ln_files[0:100]
+    pool_size = 12
+    pool = Pool(pool_size)
+    #processed = [pool.apply_async(process_file(sf)) for sf in short_files]
     processed = process_file_list(short_files)
     print len(processed)
